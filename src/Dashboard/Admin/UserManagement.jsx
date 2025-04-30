00e08699ab1fe89cf6699fdbbfbe416a6e7f4a4b
MeghlaB <meghlab964@gmail.com>
2025-04-30 23:01:02 +0600

    added

--- src/Dashboard/Admin/UserManagement.jsx
+++ src/Dashboard/Admin/UserManagement.jsx
@@ -7,7 +7,7 @@ function UserManagement() {
     const { data: users = [] } = useQuery({
       queryKey: ['doctors'],
       queryFn: async () => {
-        const res = await axiosSequire.get('/users')
+        const res = await axiosSequire.get('/users-Admin')
         console.log(res.data)
         return res.data
       }
--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -6,6 +6,7 @@ import UseAdmin from '../Hooks/UseAdmin';
 
 export default function Dashboard() {
     const [isAdmin] = UseAdmin()
+    console.log(isAdmin)
   return (
     <div>
       <div>
